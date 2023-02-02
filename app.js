92bf4c33d84a94b82f5a8d77419c472caab9e1ff
Kamilah <k.muhammad95@gmail.com>
2023-02-02 12:15:57 -0600

    exercises complete

--- app.js
+++ app.js
@@ -3,8 +3,38 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
+// Exercise 1 
+const firstName = "Kamilah";
+let lastName;
+var age;
 // Exercise 2
+lastName = "Muhammad";
+age = 27;
+
+console.log(`${firstName} ${lastName} is ${age} years old.`)
 
 // Exercise 3
+let language ="JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was created in ${createdYear}.`)
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`The customer orderd a ${selectedSize} shirt. It's price is ${price} with ${salePercentage}% off. There are now ${stock} in stock.`)
+
+//Exercise 5
+let title = "Mane in the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`It's ${hasRead} that I have read ${title} by ${author}. It has ${pageCount} pages and I have it bookmarked at page ${bookmark}.`)
